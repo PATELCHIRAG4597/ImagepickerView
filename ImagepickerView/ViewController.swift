f8b1a4a544585348cb49b5e72dce3636e6dcd500
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-04-11 18:09:21 +0530

    UIPickerView with single  Components (we have declare 7 city )

--- ImagepickerView/ViewController.swift
+++ ImagepickerView/ViewController.swift
@@ -2,18 +2,48 @@
 //  ViewController.swift
 //  ImagepickerView
 //
-//  Created by CodeInfoWay CodeInfoWay on 4/11/23.
+//  Created  CodeInfoWay CodeInfoWay on 4/11/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var waterparkTxt: UITextField!
+    
+var cityList = ["ahmeadabad","surat","pune","rajkot","mumbai","chennai","banglore"]
 
+let cityPicker = UIPickerView()
+let toolbar = UIToolbar()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cityPicker.delegate = self
+        cityPicker.dataSource = self
+        waterparkTxt.inputView = cityPicker
+        toolbar.sizeToFit()
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonePressed))
+        toolbar.items = [doneBtn]
+        waterparkTxt.inputAccessoryView = toolbar
+        
+    }
+        @objc func doneButtonePressed()
+    {
+        let SelectedCities = cityList[cityPicker.selectedRow(inComponent: 0)]
+        waterparkTxt.text = SelectedCities
+        self.view.endEditing(true)
     }
 
-
 }
-
+extension ViewController :UIPickerViewDelegate,UIPickerViewDataSource{
+         func numberOfComponents(in pickerView: UIPickerView) -> Int {
+         return 1
+   }
+ // how much number of rows in perticular selaction
+         func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+         return cityList.count
+}
+  //data display in row
+         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+         return cityList[row]
+    }
+    
+}
